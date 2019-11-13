97647e8e4920feb7bc5ab99a6fae421b4a36ff38
Apiyo4 <odingoannette@gmail.com>
2019-11-13 16:33:31 +0300

    Add a handle change function

--- src/components/MemberForm.js
+++ src/components/MemberForm.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function MemberForm(props){
+    console.log(props);
+    const [member, setMember] = useState({
+        name: '',
+        email: '',
+        role: ''
+    })
+    const handleChange = e =>{
+        
+        setMember({...member, 
+            [e.target.name]: e.target.value
+        })
+    }
+    
     return(
         <div>
             <h2>Add a member</h2>
             <form>
                 <label> Name:
-                <input type='text' placeholder='Enter your Name' name='name' id='name' />
+                <input type='text' placeholder='Enter your Name' name='name' id='name' onChange={handleChange} />
                 </label>
                 <br />
                 <label> Email:
-                <input type='text' placeholder='Enter your Email' name='email' id='email' />
+                <input type='text' placeholder='Enter your Email' name='email' id='email' onChange={handleChange} />
                 </label>
                 <br />
                 <label> Role:
-                <input type='text' placeholder='role' name='role' id='role' />
+                <input type='text' placeholder='Enter your Role' name='role' id='role' onChange={handleChange} />
                 </label>
                 <br />
                 <button type='submit'>Add Member</button>
