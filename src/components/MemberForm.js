e27b6f706720d66c8cd3b9c0f046b34b8426d7cb
Apiyo4 <odingoannette@gmail.com>
2019-11-13 16:21:44 +0300

    Create a form in MemberForm component

--- src/components/MemberForm.js
+++ src/components/MemberForm.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 function MemberForm(props){
     return(
-        <h2>Add a member</h2>
+        <div>
+            <h2>Add a member</h2>
+            <form>
+                <label> Name:
+                <input type='text' placeholder='Enter your Name' name='name' id='name' />
+                </label>
+                <br />
+                <label> Email:
+                <input type='text' placeholder='Enter your Email' name='email' id='email' />
+                </label>
+                <br />
+                <label> Role:
+                <input type='text' placeholder='role' name='role' id='role' />
+                </label>
+                <br />
+                <button type='submit'>Add Member</button>
+            </form>
+        </div>
     )
 }
 export default MemberForm;
